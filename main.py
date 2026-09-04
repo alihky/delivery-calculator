b70ad5c085c6a61c5cc56b262d21a0959572e154
Ali Kasim <alihky22@gmail.com>
2026-09-05 00:29:25 +0200

    aggiornamento del codice

--- main.py
+++ main.py
@@ -10,63 +10,52 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 class DeliveryCalculation(BaseModel):
-    month: int = Field(...,ge=1, le=12)                       # الشهر (رقم من 1 إلى 12)
-    total_income: float = Field(..., ge=0)             # إجمالي المدخول
+    how_many_months: int = Field(..., ge=1, le=12)
+    total_income: float = Field(..., ge=0)
     tax_rate: Literal[5, 15] = Field(...)
-    ateco_code: Literal['1', '2'] = Field(...)                   # كود الأتيكو (لتحديد نسبة الـ INPS لاحقاً)
-    car_expenses_per_day: float= Field(..., ge=0)            # مصاريف السيارة الشهرية
-    how_many_days_worked_this_month: int = Field(..., ge=0, le=31)
-    car_insurance: Optional[float] = None  # تأمين السيارة السنوي (اختياري)
+    ateco_code: Literal['82.99.99', '53.20.00'] = Field(...)
+    car_expenses_per_day: Optional[float] = Field(0, ge=0)  # مصاريف النقل اليومية (اختيارية)
+    how_many_days_worked: int = Field(..., ge=0, le=365)
+    car_insurance: Optional[float] = None
 
 @app.get("/", response_class=HTMLResponse)
 def get_form(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
-
 @app.post("/calculation")
 async def calculation_net_income(data: DeliveryCalculation):
 
+    # خصم قيمة الضريبة
+    percent_without_cost = 0.67
+    income = data.total_income * percent_without_cost
+    tax_amount = income * (data.tax_rate / 100)
 
-# خصم قيمة الضريبة
-        percent_without_cost = 0.67
-        income = data.total_income * percent_without_cost
-        tax_amount = income * (data.tax_rate / 100)
+    # خصم قيمة الانبس الضمان الاجتماعي
+    if data.ateco_code == '53.20.00':
+        inps = income * (26.07 / 100)
+    elif data.ateco_code == '82.99.99':
+        inps = 266.66 * data.how_many_months
+    else:
+        inps = 0.0
 
+    # تأمين السيارة
+    monthly_car_insurance = (data.car_insurance / 12) if data.car_insurance else 0
 
-# خصم قيمة الانبس الضمان الاجتماعي
+    # مصاريف النقل اليومية (إذا تركناها فارغة تكون صفراً)
+    daily_expenses = data.car_expenses_per_day if data.car_expenses_per_day else 0
+    car_expenses = data.how_many_days_worked * daily_expenses
 
-        if data.ateco_code == '1':
-            inps = income * (26.07 / 100)
+    # صافي الربح
+    Total_expenses = (tax_amount + inps + car_expenses
+                      + (monthly_car_insurance * data.how_many_months))
+    Net_income = data.total_income - Total_expenses
 
-        elif data.ateco_code == '2':
-            inps = 243.75 # القيمة الثابتة الشهرية (2925 سنوية مقسومة على 12)
-
-        else:
-            inps = 0.0
-# تامين السيارة
-        monthly_car_insurance = (data.car_insurance / 12) if data.car_insurance else 0
-
-# مصاريف السيارة الشهرية
-
-        car_expenses = data.how_many_days_worked_this_month * data.car_expenses_per_day
-
-# صافي الربح
-
-        Total_expenses = (tax_amount + inps + car_expenses
-                          + monthly_car_insurance)
-        Net_income = data.total_income - Total_expenses
-# ارجاع النتائج
-
-        return {
-            "total_income": data.total_income,
-            "tax_amount": round(tax_amount, 2),
-            "inps_amount": round(inps, 2),
-            "car_expenses": round(car_expenses, 2),
-            "total_expenses": round(Total_expenses, 2),
-            "net_income": round(Net_income, 2)
-        }
-
-
-
-
-
+    # ارجاع النتائج
+    return {
+        "total_income": data.total_income,
+        "tax_amount": round(tax_amount, 2),
+        "inps_amount": round(inps, 2),
+        "car_expenses": round(car_expenses, 2),
+        "total_expenses": round(Total_expenses, 2),
+        "net_income": round(Net_income, 2)
+    }
